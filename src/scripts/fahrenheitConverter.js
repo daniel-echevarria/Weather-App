36cfbbc3ea8644eb8d81e75fa55d7d88f1bc4cc5
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-07-18 13:24:08 +0200

    Improve design

--- src/scripts/fahrenheitConverter.js
+++ src/scripts/fahrenheitConverter.js
@@ -1,10 +1,9 @@
 export default function fahrenheitConverter(temp) {
   const tempInCelsius = ((temp - 32) * 5) / 9;
-  return Math.round(tempInCelsius);
+  return Math.round(tempInCelsius * 10) / 10;
 }
 
 export function celciusConverter(temp) {
   const tempInFahrenheit = (temp * 9) / 5 + 32;
-  console.log(tempInFahrenheit);
-  return Math.round(tempInFahrenheit);
+  return Math.round(tempInFahrenheit * 10) / 10;
 }
